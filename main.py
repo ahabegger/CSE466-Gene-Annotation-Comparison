bb921f5a68d55acfc0599a338b1b950056685c38
Alex Habegger <habeggaj@miamioh.edu>
2021-11-11 20:11:12 +0000

    Upload a main python basebones

---
+++ main.py
@@ -0,0 +1,16 @@
+# Final CSE 466 Project
+# Alex Habegger and Lily Hofman
+
+
+# Main Function
+if __name__ == "__main__":
+    # Intro
+    print("********************************************************************************************")
+    print("Final CSE 466 Project")
+    print("********************************************************************************************")
+
+    # Program Information print
+    print("\nProgram       : {0}".format("Gene Annotation Comparison"))
+    print("Developer Name: {0}".format("Alex Habegger and Lily Hofman"))
+    print("Developer ID  : {0}".format("habeggaj(Alex), hofmanln(Lily)"))
+
